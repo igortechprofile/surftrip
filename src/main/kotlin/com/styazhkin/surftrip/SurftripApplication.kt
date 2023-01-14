daf09bd02a4cd66da44c95079bf32c35b8be1e9c
Igor Styazhkin <styazhkin.igor@gmail.com>
2023-01-15 00:45:19 +0100

    Add dummy RestController

--- src/main/kotlin/com/styazhkin/surftrip/SurftripApplication.kt
+++ src/main/kotlin/com/styazhkin/surftrip/SurftripApplication.kt
@@ -2,9 +2,17 @@ package com.styazhkin.surftrip
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class SurftripApplication
+@RestController
+class SurftripApplication(){
+	@RequestMapping("/")
+	fun home(): String {
+		return "Hello from Spring App running in Docker!"
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<SurftripApplication>(*args)
